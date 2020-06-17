d949ff68923b30ba2a700c3da7eea9aefa27d6fb
kingavatar <saikiranreddy06@gmail.com>
2020-06-17 18:59:59 +0530

    Adding Tabulat Data plus Serach Field and Tooltips

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -3,17 +3,32 @@
         <div class="drawer">
             <v-list flat nav>
                 <v-list-item to="/" color="primary" class="px-8">
-                    <v-icon large>mdi-view-dashboard</v-icon>
+                    <v-tooltip right>
+                        <template v-slot:activator="{ on, attrs }">
+                            <v-icon large v-bind="attrs" v-on="on">mdi-view-dashboard</v-icon>
+                        </template>
+                        <span>DashBoard</span>
+                    </v-tooltip>
                 </v-list-item>
             </v-list>
             <v-list flat nav>
                 <v-list-item to="/search" color="primary" class="px-8">
-                    <v-icon large>mdi-magnify</v-icon>
+                    <v-tooltip right>
+                        <template v-slot:activator="{ on, attrs }">
+                            <v-icon large v-bind="attrs" v-on="on">mdi-magnify</v-icon>
+                        </template>
+                        <span>Advanced Search</span>
+                    </v-tooltip>
                 </v-list-item>
             </v-list>
             <v-list flat nav>
                 <v-list-item to="/settings" color="primary" class="px-8">
-                    <v-icon large>mdi-cog</v-icon>
+                    <v-tooltip right>
+                        <template v-slot:activator="{ on, attrs }">
+                            <v-icon large v-bind="attrs" v-on="on">mdi-cog</v-icon>
+                        </template>
+                        <span>Settings</span>
+                    </v-tooltip>
                 </v-list-item>
             </v-list>
         </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,28 +2,62 @@
     <div class="home">
         <v-container fluid>
             <v-row>
+                <h1 class="pa-6">Dashboard</h1>
                 <v-col cols="12" class="ma-3 pa-6">
-                    <h1 class="pb-8">This is Home Page</h1>
-
                     <v-card shaped height="100%" width="100%">
-                        <v-list shaped>
+                        <!-- <v-list shaped>
                             <v-subheader>VEHICLE HISTORY</v-subheader>
                             <v-list-item-group v-model="item" color="primary">
-                                <v-list-item v-for="(item, i) in items" :key="i">
+                                <v-list-item
+                                    v-for="(item, i) in items"
+                                    :key="i"
+                                >
                                     <v-list-item-icon>
                                         <v-icon v-text="item.icon"></v-icon>
                                     </v-list-item-icon>
                                     <v-list-item-content>
                                         <v-col cols="auto" class="mr-auto">
-                                            <v-list-item-title v-text="item.vehicleNo"></v-list-item-title>
+                                            <v-list-item-title
+                                                v-text="item.vehicleNo"
+                                            ></v-list-item-title>
                                         </v-col>
                                         <v-col cols="auto">
-                                            <v-list-item-title v-text="item.time"></v-list-item-title>
+                                            <v-list-item-title
+                                                v-text="item.time"
+                                            ></v-list-item-title>
                                         </v-col>
                                     </v-list-item-content>
                                 </v-list-item>
                             </v-list-item-group>
-                        </v-list>
+                        </v-list>-->
+                        <v-card-title>
+                            Vehicle History
+                            <v-spacer></v-spacer>
+                            <v-text-field
+                                v-model="search"
+                                append-icon="mdi-magnify"
+                                label="Search"
+                                single-line
+                                hide-details
+                            ></v-text-field>
+                        </v-card-title>
+                        <v-data-table
+                            :headers="headers"
+                            :items="vehicles"
+                            :items-per-page="10"
+                            :search="search"
+                            class="elevation-1"
+                        >
+                            <template v-slot:item.icon="{ item }">
+                                <v-icon>{{ item.icon }}</v-icon>
+                            </template>
+                            <template v-slot:item.action="{ item }">
+                                <v-chip
+                                    :color="getColor(item.action)"
+                                    :text-color="getTextColor(item.action)"
+                                >{{ item.action }}</v-chip>
+                            </template>
+                        </v-data-table>
                     </v-card>
                 </v-col>
             </v-row>
@@ -38,7 +72,7 @@ export default {
     name: 'Home',
     components: {},
     data: () => ({
-        item: 1,
+        item: 0,
         items: [
             {
                 vehicleNo: 'Visitor Vehicle',
@@ -50,12 +84,60 @@ export default {
                 time: 'TimeStamp',
                 icon: 'mdi-account'
             },
+
             {
                 vehicleNo: 'Commercial Vehicle',
                 time: 'TimeStamp',
                 icon: 'mdi-flag'
             }
+        ],
+        search: '',
+        headers: [
+            { align: 'start', sortable: false, value: 'icon' },
+            {
+                text: 'Vehicle Number',
+                value: 'number'
+            },
+            { text: 'Type', value: 'type' },
+            { text: 'Action', value: 'action' },
+            { text: 'TimeStamp', value: 'time' }
+        ],
+        vehicles: [
+            {
+                action: 'Entry',
+                number: 'XXXXXX',
+                type: 'Visitor Vehicle',
+                time: 'TimeStamp',
+                icon: 'mdi-clock'
+            },
+            {
+                action: 'Exit',
+                number: 'XXXXXX',
+                type: 'Resident Vehicle',
+                time: 'TimeStamp',
+                icon: 'mdi-account'
+            },
+
+            {
+                action: 'Entry',
+                number: 'XXXXXX',
+                type: 'Commercial Vehicle',
+                time: 'TimeStamp',
+                icon: 'mdi-flag'
+            }
         ]
-    })
+    }),
+    methods: {
+        getColor(action) {
+            if (action === 'Entry') return '#F3FEF3';
+            else if (action === 'Exit') return '#FEF5F8';
+            else return '#FFF9F4';
+        },
+        getTextColor(action) {
+            if (action === 'Entry') return 'green';
+            else if (action === 'Exit') return 'red';
+            else return 'orange';
+        }
+    }
 };
 </script>
