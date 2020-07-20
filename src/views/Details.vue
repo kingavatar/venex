6f48a22abe67e721542444a0697433a651297b0d
kingavatar <saikiranreddy06@gmail.com>
2020-07-20 16:45:19 +0530

    Adding Details Page

---
+++ src/views/Details.vue
@@ -0,0 +1,85 @@
+<template>
+    <h1>Account Page</h1>
+</template>
+
+<script>
+export default {};
+// headers: [
+//             { align: 'start', sortable: false, value: 'icon' },
+//             {
+//                 text: 'Vehicle Number',
+//                 value: 'number'
+//             },
+//             { text: 'Type', value: 'type' },
+//             { text: 'Action', value: 'action' },
+//             { text: 'TimeStamp', value: 'time' }
+//         ],
+//         vehicles: [
+//             {
+//                 action: 'Entry',
+//                 number: 'XXXXXX',
+//                 type: 'Visitor Vehicle',
+//                 time: 'TimeStamp',
+//                 icon: 'mdi-clock'
+//             },
+//             {
+//                 action: 'Exit',
+//                 number: 'XXXXXX',
+//                 type: 'Resident Vehicle',
+//                 time: 'TimeStamp',
+//                 icon: 'mdi-account'
+//             },
+
+//             {
+//                 action: 'Entry',
+//                 number: 'XXXXXX',
+//                 type: 'Commercial Vehicle',
+//                 time: 'TimeStamp',
+//                 icon: 'mdi-flag'
+//             },
+//             {
+//                 action: 'Entry',
+//                 number: 'XXXXXX',
+//                 type: 'Visitor Vehicle',
+//                 time: 'TimeStamp',
+//                 icon: 'mdi-clock'
+//             },
+//             {
+//                 action: 'Exit',
+//                 number: 'XXXXXX',
+//                 type: 'Resident Vehicle',
+//                 time: 'TimeStamp',
+//                 icon: 'mdi-account'
+//             },
+
+//             {
+//                 action: 'Entry',
+//                 number: 'XXXXXX',
+//                 type: 'Commercial Vehicle',
+//                 time: 'TimeStamp',
+//                 icon: 'mdi-flag'
+//             },
+//             {
+//                 action: 'Entry',
+//                 number: 'XXXXXX',
+//                 type: 'Visitor Vehicle',
+//                 time: 'TimeStamp',
+//                 icon: 'mdi-clock'
+//             },
+//             {
+//                 action: 'Exit',
+//                 number: 'XXXXXX',
+//                 type: 'Resident Vehicle',
+//                 time: 'TimeStamp',
+//                 icon: 'mdi-account'
+//             },
+
+//             {
+//                 action: 'Entry',
+//                 number: 'XXXXXX',
+//                 type: 'Commercial Vehicle',
+//                 time: 'TimeStamp',
+//                 icon: 'mdi-flag'
+//             }
+//         ]
+</script>
