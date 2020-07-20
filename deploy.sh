7140772ba28d7ab2d34d45128fb350223addb688
kingavatar <saikiranreddy06@gmail.com>
2020-07-21 01:40:17 +0530

    Changing Deploy Code to gh-pages

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,38 @@
-#!/bin/sh
+#!/bin/bash
 
-# abort on errors
-set -e
+directory=dist
 
-git checkout gh-pages
+branch=gh-pages
 
-git rebase master
-# build
-npm run build
+build_command() {
 
-git add dist && git commit -m 'New Dist Subtree Commit'
+  npm run build
 
-git subtree push --prefix dist origin gh-pages
+}
 
-git checkout master
+
+echo -e "\033[0;32mDeleting old content...\033[0m"
+rm -rf $directory
+
+echo -e "\033[0;32mGenerating site...\033[0m"
+build_command
+
+echo -e "\033[0;32mChanging Directory...\033[0m"
+mv $directory $directory\_new
+
+echo -e "\033[0;32mChecking out $branch....\033[0m"
+git worktree add $directory $branch
+
+echo -e "\033[0;32mCopying Build Files and deleting Extra Directory...\033[0m"
+cp -r $directory\_new/* $directory
+rm -rf $directory\_new
+
+echo -e "\033[0;32mDeploying $branch branch...\033[0m"
+cd $directory &&
+  git add --all &&
+  git commit -m "Build output as of $(git log '--format=format:%H' master -1)" &&
+  git push origin $branch
+
+
+echo -e "\033[0;32mCleaning up...\033[0m"
+git worktree remove $directory
