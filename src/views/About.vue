40ba039c95e94aad39bbb8f4812d76e36a5289c0
Vishnu Lahari <32712308+Lahari-5139@users.noreply.github.com>
2020-08-01 12:13:12 +0530

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -6,7 +6,7 @@
             Venex is an automated surveillance system. It is effective and
             efficient in monitoring of vehicles
         </p>
-        <img src="sih.jpeg" alt="SIH Logo" />
+        <img src="./sih.jpeg" alt="SIH Logo" width = "100" height = "100"/>
         <a href="https://www.sih.gov.in/">Visit sih.gov.in</a>
     </div>
 </template>
