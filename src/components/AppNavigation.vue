da22335089c9753d5c2c1f1f5cc394873bd14020
Vishnu Lahari <32712308+Lahari-5139@users.noreply.github.com>
2020-08-01 11:29:18 +0530

    changed nothing

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -86,14 +86,14 @@
                 </v-list-item>
             </v-list>
             <v-list flat nav>
-                <v-list-item to="/about" color="primary" class="px-6 my-6">
+                <v-list-item to="/setting" color="primary" class="px-6 my-6">
                     <v-tooltip right>
                         <template v-slot:activator="{ on, attrs }">
                             <v-icon large v-bind="attrs" v-on="on"
                                 >mdi-cog</v-icon
                             >
                         </template>
-                        <span>About</span>
+                        <span>Settings</span>
                     </v-tooltip>
                 </v-list-item>
             </v-list>
